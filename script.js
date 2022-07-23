08d2631466d48f457725d32efffc99aa4ef06cc3
Athul_Rajagopal <athulhash1408@gmail.com>
2022-07-23 16:22:21 +0530

    a home page and a javascript file included. the calculator is ready for use.

---
+++ script.js
@@ -0,0 +1,15 @@
+function buttonClick(val){
+    console.log(val)
+    document.getElementById("screen").value+=val;
+}
+
+function clearDisplay(){
+    document.getElementById("screen").value=""
+}
+
+function equalTo(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+
+}
